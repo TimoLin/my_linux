df097e409dce26d5a4b19818ffd7a32a9584be70
ztnuaa <1354434853@qq.com>
2023-05-12 15:38:21 +0800

    Check whhich user (root/normal) we are using and the do different stuff.

--- script/centos7.sh
+++ script/centos7.sh
@@ -1,46 +1,47 @@
 # CentOS Environment deployment
 
 # 1. Change Software source and update
-su # Get root acess
-## Base repo
-sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-    -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.ustc.edu.cn/centos|g' \
-    -i.bak \
-    /etc/yum.repos.d/CentOS-Base.repo
-yum makecache
-## EPEL repo
-yum install -y epel-release
-sed -e 's|^metalink=|#metalink=|g' \
-         -e 's|^#baseurl=https\?://download.fedoraproject.org/pub/epel/|baseurl=https://mirrors.ustc.edu.cn/epel/|g' \
-         -e 's|^#baseurl=https\?://download.example/pub/epel/|baseurl=https://mirrors.ustc.edu.cn/epel/|g' \
-         -i.bak \
-         /etc/yum.repos.d/epel.repo
-yum makecache
-
-## Update system
-yum -y update
-
-# 2. VNC server
-# Install VNC server and ntfs support
-yum install -y ntfs-3g tigervnc tigervnc-server
-
-## Configuration of VNC server
-wget https://ghproxy.com/https://raw.githubusercontent.com/TimoLin/my_linux/master/script/vnc.service
-sed -i "s/USERNAME/$USER/" vnc.service
-mv ./vnc.service /etc/systemd/system/vncserver@\:1.service
-systemctl daemon-reload
-systemctl enable vncserver@:1.service
-
-## Set firewall for vnc port
-firewall-cmd --permanent --zone=public --add-port=5901-5905/tcp
-firewall-cmd --reload
+if (( $EUID == 0 )); then
+    ## Base repo
+    sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+        -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.ustc.edu.cn/centos|g' \
+        -i.bak \
+        /etc/yum.repos.d/CentOS-Base.repo
+    yum makecache
+    ## EPEL repo
+    yum install -y epel-release
+    sed -e 's|^metalink=|#metalink=|g' \
+             -e 's|^#baseurl=https\?://download.fedoraproject.org/pub/epel/|baseurl=https://mirrors.ustc.edu.cn/epel/|g' \
+             -e 's|^#baseurl=https\?://download.example/pub/epel/|baseurl=https://mirrors.ustc.edu.cn/epel/|g' \
+             -i.bak \
+             /etc/yum.repos.d/epel.repo
+    yum makecache
+    
+    ## Update system
+    yum -y update
+    
+    # 2. VNC server
+    # Install VNC server and ntfs support
+    yum install -y ntfs-3g tigervnc tigervnc-server
+    
+    ## Configuration of VNC server
+    wget https://ghproxy.com/https://raw.githubusercontent.com/TimoLin/my_linux/master/script/vnc.service
+    sed -i "s/USERNAME/$USER/" vnc.service
+    mv ./vnc.service /etc/systemd/system/vncserver@\:1.service
+    systemctl daemon-reload
+    systemctl enable vncserver@:1.service
+    
+    ## Set firewall for vnc port
+    firewall-cmd --permanent --zone=public --add-port=5901-5905/tcp
+    firewall-cmd --reload
+fi
 
 # 3. Fetch bashrc
-# Exit root
-exit
-cd $HOME
-# get bashrc
-wget -O .bashrc https://ghproxy.com/https://raw.githubusercontent.com/TimoLin/my_linux/master/script/vnc.service
+if (( $EUID != 0 )); then
+    cd $HOME
+    # get bashrc
+    wget -O .bashrc https://ghproxy.com/https://raw.githubusercontent.com/TimoLin/my_linux/master/script/vnc.service
+fi
 
 # OpenFoam
 #yum groupinstall 'Development Tolls' --setopt=group_package_types=mandatory,default,optional
